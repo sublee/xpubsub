035dd84a226f1cd363cd5279bca6fdd342e0ad9b
Heungsub Lee <sub@subl.ee>
2014-04-13 17:58:19 +0900

    add test

---
+++ zforwarder.go
@@ -0,0 +1,14 @@
+package zforwarder
+
+import zmq "github.com/pebbe/zmq4"
+
+func Proxy(frontend *zmq.Socket, backend *zmq.Socket, errChan chan error) {
+	errChan <- zmq.Proxy(frontend, backend, nil)
+}
+
+func Forwarder(xpub *zmq.Socket, xsub *zmq.Socket) error {
+	errChan := make(chan error)
+	go Proxy(xpub, xsub, errChan)
+	go Proxy(xsub, xpub, errChan)
+	return <-errChan
+}
---
+++ zforwarder_test.go
@@ -0,0 +1,35 @@
+package zforwarder_test
+
+import (
+	"time"
+	zmq "github.com/pebbe/zmq4"
+	zforwarder "github.com/sublee/zforwarder"
+	"testing"
+)
+
+func TestProxy(t *testing.T) {
+	// set sockets
+	pub, _ := zmq.NewSocket(zmq.PUB)
+	sub, _ := zmq.NewSocket(zmq.SUB)
+	xpub, _ := zmq.NewSocket(zmq.XPUB)
+	xsub, _ := zmq.NewSocket(zmq.XSUB)
+	xpub.Bind("inproc://xpub")
+	xsub.Bind("inproc://xsub")
+	pub.Connect("inproc://xsub")
+	sub.Connect("inproc://xpub")
+	// run forwarder
+	go zforwarder.Forwarder(xpub, xsub)
+	// communicate between PUB/SUB via XPUB/XSUB
+	sub.SetSubscribe("")
+	time.Sleep(time.Nanosecond)
+	msgExpected := "Hello"
+	pub.Send(msgExpected, 0)
+	msg, err := sub.Recv(0)
+	// check result
+	if err != nil {
+		t.Fatalf("An error occurs by SUB socket: %s", err)
+	}
+	if msg != msgExpected {
+		t.Fatalf("Received message: %s", msg)
+	}
+}
